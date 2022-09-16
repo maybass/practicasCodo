2d46d6a4331a17751f3998240c3d806da13ecd08
maybass <103843894+maybass@users.noreply.github.com>
2022-09-16 14:30:11 -0300

    Update random.js

--- random.js
+++ random.js
@@ -1,3 +1,4 @@
+// perder cuando el ingresado es distinto al aleatorio
 function ganarPerder() {
     let intentos = 3;
     while(intentos!==0) {
@@ -13,6 +14,27 @@ function ganarPerder() {
 }
 
 ganarPerder()
+
+/* perder cuando el ingresado es mayor o menor que el aleatorio
+function ganarPerder() {
+    let intentos = 3;
+    while(intentos!==0) {
+        let alea = Math.round(Math.random()*5) //del 0 al 4
+        let numero = Number(prompt("Ingresa un numero del 0 al 4"))
+        if(numero === alea) {
+            alert("Ganaste " + "el numero aleatorio era " + alea + ' y tus intentos son ' + intentos)
+        }else if(numero< alea) {
+            intentos --
+            alert("Perdiste tu numero es menor te quedan " + intentos +' intentos el numero aleatorio era ' + alea)
+        }else if(numero>alea) {
+            intentos --
+            alert("Perdiste tu numero es mayor te quedan " + intentos +' intentos el numero aleatorio era ' + alea)
+            
+        }
+}
+}
+
+ganarPerder() */
 
 
 
